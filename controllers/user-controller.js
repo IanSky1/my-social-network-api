0130f33e48a6ef4990074a60b304aaef14af41a5
Ian <Ian.Mohn1@gmail.com>
2022-07-04 01:54:04 -0500

    updated methods in user-controller.js file, added the addFriend method

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -37,5 +37,31 @@ const userController = {
             .catch((err) => res.json(err))
     },
 
-    updateUser
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404),json({message: "Sorry! No user with that id has been found"});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({_id: params.id})
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.json(err));
+    },
+
+    addFriend({params}, res) {
+        User.findOneAndUpdate(
+            {_id: params.id},
+            {$push: {friends: params.friendsId}},
+            {new: true}
+        )
+    }
+
 }
+
