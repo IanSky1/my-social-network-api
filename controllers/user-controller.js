80c52e5fe5f495894b0007ae184c350d8d749d24
Ian <Ian.Mohn1@gmail.com>
2022-07-04 01:16:20 -0500

    created 3 initial methods for userController.js

---
+++ controllers/user-controller.js
@@ -0,0 +1,41 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({ path: "thoughts", select: "-__v" })
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__v")
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+            .populate({
+                path: "thoughts",
+                select: "-__v",
+            })
+            .populate({
+                path: "friends",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.json(err))
+    },
+
+    updateUser
+}
