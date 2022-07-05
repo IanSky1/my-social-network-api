1e042b5ac11450e96f8d332dfda0163476e55c9d
Ian <Ian.Mohn1@gmail.com>
2022-07-04 22:19:46 -0500

    changed port to localhost for testing insomnia

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/my-social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/my-social-network-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+}
+);
 
 mongoose.set('debug', true);
 
